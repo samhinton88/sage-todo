a8c3f315acbe15d2803c4a8b1b84962206d5c49f
Sam Hinton <sam.hinton88@gmail.com>
2025-03-09 16:16:21 +0000

    feat: action buttons on todo items

--- src/components/TodoItem/TodoItem.spec.tsx
+++ src/components/TodoItem/TodoItem.spec.tsx
@@ -14,4 +14,42 @@ describe("TodoItem", () => {
 
     expect(await screen.findByText("Something to be done")).toBeInTheDocument();
   });
+
+  test("renders a only mark as in progress button when PENDING", async () => {
+    const todo = {
+      id: "some-unique-id",
+      content: "Something to be done",
+      status: "PENDING" as TodoStatus,
+    };
+    render(<TodoItem todo={todo} />);
+
+    expect(
+      await screen.getByRole("button", { name: "Mark as In Progress" })
+    ).toBeInTheDocument();
+    expect(
+      await screen.queryByRole("button", { name: "Mark as Pending" })
+    ).not.toBeInTheDocument();
+    expect(
+      await screen.queryByRole("button", { name: "Mark as Complete" })
+    ).not.toBeInTheDocument();
+  });
+
+  test("renders a only return to pending and mark as complete buttons when IN PROGRESS", async () => {
+    const todo = {
+      id: "some-unique-id",
+      content: "Something to be done",
+      status: "IN PROGRESS" as TodoStatus,
+    };
+    render(<TodoItem todo={todo} />);
+
+    expect(
+      await screen.getByRole("button", { name: "Return to Pending" })
+    ).toBeInTheDocument();
+    expect(
+      await screen.getByRole("button", { name: "Mark as Complete" })
+    ).toBeInTheDocument();
+    expect(
+      await screen.queryByRole("button", { name: "Mark as In Progress" })
+    ).not.toBeInTheDocument();
+  });
 });
--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,6 +1,46 @@
+import Button from "carbon-react/lib/components/button";
 import { Todo } from "../../shared/types";
 import Box from "carbon-react/lib/components/box";
+import Typography from "carbon-react/lib/components/typography";
 
-export const TodoItem = ({ todo }: { todo: Todo }) => {
-  return <Box>{todo.content}</Box>;
+export const TodoItem = ({
+  todo,
+  onClickMarkInProgress,
+  onClickMarkComplete,
+  onClickMarkPending,
+}: {
+  todo: Todo;
+  onClickMarkInProgress?: () => void;
+  onClickMarkComplete?: () => void;
+  onClickMarkPending?: () => void;
+}) => {
+  return (
+    <Box>
+      <Box>
+        <Typography>{todo.content}</Typography>
+      </Box>
+      <Box flexDirection={"row"}>
+        {todo.status === "PENDING" && (
+          <Button size="small" onClick={onClickMarkInProgress}>
+            Mark as In Progress
+          </Button>
+        )}
+        {todo.status === "IN PROGRESS" && (
+          <>
+            <Button size="small" onClick={onClickMarkPending}>
+              Return to Pending
+            </Button>
+            <Button size="small" onClick={onClickMarkComplete}>
+              Mark as Complete
+            </Button>
+          </>
+        )}
+        {todo.status === "COMPLETE" && (
+          <Button size="small" onClick={onClickMarkPending}>
+            Mark as Pending
+          </Button>
+        )}
+      </Box>
+    </Box>
+  );
 };
