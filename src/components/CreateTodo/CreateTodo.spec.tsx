4155e3ed0a47ff27e75921ace66093ad210fbd63
Sam Hinton <sam.hinton88@gmail.com>
2025-03-09 14:08:50 +0000

    feat: create todo component mvp and spec

---
+++ src/components/CreateTodo/CreateTodo.spec.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import { describe, expect, test, vi } from "vitest";
+import { CreateTodo } from "./CreateTodo";
+
+describe("<CreateTodo/>", () => {
+  test("renders a form", async () => {
+    // Provide an empty mock for onSubmit
+    render(<CreateTodo onSubmit={() => {}} />);
+
+    // Check for the labeled textarea and the buttons
+    expect(screen.getByLabelText("Content")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /cancel/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /save/i })).toBeInTheDocument();
+  });
+
+  test("captures data to create a todo on submit", () => {
+    const onSubmitMock = vi.fn();
+    render(<CreateTodo onSubmit={onSubmitMock} />);
+
+    const textarea = screen.getByLabelText("Content");
+    fireEvent.change(textarea, { target: { value: "my new todo" } });
+
+    fireEvent.click(screen.getByRole("button", { name: /save/i }));
+
+    expect(onSubmitMock).toHaveBeenCalledTimes(1);
+    expect(onSubmitMock).toHaveBeenCalledWith({ content: "my new todo" });
+  });
+});
---
+++ src/components/CreateTodo/CreateTodo.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Form from "carbon-react/lib/components/form";
+import Button from "carbon-react/lib/components/button";
+import Textarea from "carbon-react/lib/components/textarea";
+import { TodoPayload } from "../../shared/types";
+
+export const CreateTodo = ({
+  onSubmit,
+}: {
+  onSubmit: (data: TodoPayload) => void;
+}) => {
+  console.log("create todo");
+  const [value, setValue] = useState("");
+  const handleValueChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(target.value);
+  };
+
+  return (
+    <Form
+      onSubmit={(d) => {
+        d.preventDefault();
+        console.log(d);
+        onSubmit({ content: value });
+      }}
+      leftSideButtons={
+        <Button onClick={() => console.log("cancel")}>Cancel</Button>
+      }
+      saveButton={
+        <Button buttonType="primary" type="submit">
+          Save
+        </Button>
+      }
+      stickyFooter
+    >
+      <Textarea label="Content" value={value} onChange={handleValueChange} />
+    </Form>
+  );
+};
